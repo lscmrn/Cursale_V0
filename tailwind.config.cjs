8dac6f3f70751f55cf088ce123ee8a8a202c4294
lscmrn <laiscamarini5@gmail.com>
2025-07-22 20:11:09 -0300

    new settings

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,22 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jakarta: ['Plus Jakarta Sans', 'sans-serif'],
+        rubik: ['Rubik', 'Helvetica', 'Arial', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        "cursale-blue": {
+          900: "#004576",
+          700: "#2c658d",
+          500: "#5986a5",
+          300: "#85a6bd",
+          100: "#b2c7d5",
+        },
+      },
+    },
   },
   plugins: [],
 };
